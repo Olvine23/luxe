2b6ba3c2d0960646392aa3d810cddfb5b37f9561
Olvine23 <olivinegeorge@gmail.com>
2022-09-10 15:49:54 +0300

    made some changes

--- src/components/features.js
+++ src/components/features.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-  import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+ 
   import { faHouseSignal } from '@fortawesome/free-solid-svg-icons'
 
 
