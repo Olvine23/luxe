200dfda85e75393473b4da56bc7397dbde781c39
Olvine23 <olivinegeorge@gmail.com>
2022-09-10 15:36:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Nav } from "./components/navbar";
+import { Header } from "./components/header";
+import { Feature } from "./components/features";
+import { Footer } from "./components/footer";
+import { Blog } from "./components/blog";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Header />
+      <Feature />
+      <Blog />
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/features.js
@@ -0,0 +1,299 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+  import { faHouseSignal } from '@fortawesome/free-solid-svg-icons'
+
+
+export const Feature = () => {
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-2">
+        <div>
+          <p className="inline-block px-3 py-px mb- text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+            Brand new
+          </p>
+        </div>
+        <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          Weekly Deals
+        </h2>
+        <p className="text-base text-gray-700 md:text-lg">
+         Giving you the best deals in town 
+        </p>
+      </div>
+
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="grid gap-10 lg:grid-cols-2">
+        <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
+          <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
+            <svg className="text-teal-900 w-7 h-7" viewBox="0 0 24 24">
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points=" 8,5 8,1 16,1 16,5"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="9,15 1,15 1,5 23,5 23,15 15,15"
+                strokeLinejoin="round"
+              />
+              <polyline
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                points="22,18 22,23 2,23 2,18"
+                strokeLinejoin="round"
+              />
+              <rect
+                x="9"
+                y="13"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeMiterlimit="10"
+                width="6"
+                height="4"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </div>
+          <div className="max-w-xl mb-6">
+            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Let us handle
+              <br className="hidden md:block" />
+              your next{' '}
+              <span className="inline-block text-deep-purple-accent-400">
+                destination
+              </span>
+            </h2>
+            <p className="text-base text-gray-700 md:text-lg">
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae. explicabo.
+            </p>
+          </div>
+          <div>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+              <svg
+                className="inline-block w-3 ml-2"
+                fill="currentColor"
+                viewBox="0 0 12 12"
+              >
+                <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center -mx-4 lg:pl-8">
+          <div className="flex flex-col items-end px-3">
+            <img
+              className="object-cover mb-6 rounded shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56"
+              src="https://media.istockphoto.com/photos/wide-angle-real-estate-interior-shot-of-a-beautiful-trendy-aframe-picture-id1322234491?b=1&k=20&m=1322234491&s=170667a&w=0&h=aTJKqFwwM6aHtfi4rtwLRKewP_lwTCvR7u8XqNFVoPo="
+              alt=""
+            />
+            <img
+              className="object-cover w-20 h-20 rounded shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40"
+              src="https://media.istockphoto.com/photos/modern-minimalist-family-villa-picture-id1254871777?k=20&m=1254871777&s=612x612&w=0&h=bG_QudxmoVZkKpjI0wMOuUvH86yNk0K-gPZytf_TSbQ="
+              alt=""
+            />
+          </div>
+          <div className="px-3">
+            <img
+              className="object-cover w-40 h-40 rounded shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80"
+              src="https://media.istockphoto.com/photos/evening-view-of-a-modern-house-with-swimming-pool-picture-id1151833014?b=1&k=20&m=1151833014&s=170667a&w=0&h=DBLJJWjMpzOkt3Hvu3OPDHOfgYIAfNGk4qeCvKKeE1k="
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+      
+      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        
+      <div className="grid gap-10 lg:grid-cols-2">
+      <div>
+          <img
+            className="object-cover w-full h-56 rounded shadow-lg sm:h-96"
+            src="https://media.istockphoto.com/photos/evening-view-of-a-modern-house-with-swimming-pool-picture-id1151833014?b=1&k=20&m=1151833014&s=170667a&w=0&h=DBLJJWjMpzOkt3Hvu3OPDHOfgYIAfNGk4qeCvKKeE1k="
+            alt=""
+          />
+        </div>
+        <div className="lg:pr-10">
+          <a
+            href="/"
+            aria-label="Go Home"
+            title="Logo"
+            className="inline-block mb-5"
+          >
+            <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
+           
+            <FontAwesomeIcon icon= {faHouseSignal} />
+            </div>
+          </a>
+         
+          <h5 className="mb-4 text-4xl font-extrabold leading-none">
+            Property Management
+          </h5>
+          <p className="mb-6 text-gray-900">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae explicabo. Sed ut perspiciatis unde omnis iste natus error sit
+            voluptatem totam rem aperiam, eaque ipsa quae explicabo.
+          </p>
+          <hr className="mb-5 border-gray-300" />
+          <div className="flex items-center space-x-4">
+            <a
+              href="/"
+              className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z" />
+              </svg>
+            </a>
+            <a
+              href="/"
+              className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                <circle cx="15" cy="15" r="4" />
+                <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+              </svg>
+            </a>
+            <a
+              href="/"
+              className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+              </svg>
+            </a>
+            <a
+              href="/"
+              className="text-gray-600 transition-colors duration-300 hover:text-deep-purple-accent-400"
+            >
+              <svg viewBox="0 0 24 24" fill="currentColor" className="h-6">
+                <path d="M23.8,7.2c0,0-0.2-1.7-1-2.4c-0.9-1-1.9-1-2.4-1C17,3.6,12,3.6,12,3.6h0c0,0-5,0-8.4,0.2 c-0.5,0.1-1.5,0.1-2.4,1c-0.7,0.7-1,2.4-1,2.4S0,9.1,0,11.1v1.8c0,1.9,0.2,3.9,0.2,3.9s0.2,1.7,1,2.4c0.9,1,2.1,0.9,2.6,1 c1.9,0.2,8.2,0.2,8.2,0.2s5,0,8.4-0.3c0.5-0.1,1.5-0.1,2.4-1c0.7-0.7,1-2.4,1-2.4s0.2-1.9,0.2-3.9v-1.8C24,9.1,23.8,7.2,23.8,7.2z M9.5,15.1l0-6.7l6.5,3.4L9.5,15.1z" />
+              </svg>
+            </a>
+          </div>
+        </div>
+       
+      </div>
+    </div>
+      <div className="grid max-w-screen-lg gap-8 row-gap-10 mx-auto lg:grid-cols-2">
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+               
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">The deep ocean</h6>
+            <p className="mb-3 text-sm text-gray-900">
+              A flower in my garden, a mystery in my panties. Heart attack never
+              stopped old Big Bear. I didn't even know we were calling him Big
+              Bear. We never had the chance to.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+                
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              When has justice
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Rough pomfret lemon shark plownose chimaera southern sandfish
+              kokanee northern sea robin Antarctic cod. Yellow-and-black
+              triplefin gulper South American Lungfish mahi-mahi, butterflyfish
+              glass catfish soapfish ling gray mullet!
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+               
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              Organically grow
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              A slice of heaven. O for awesome, this chocka full cuzzie is as
+              rip-off as a cracker. Meanwhile, in behind the bicycle shed,
+              Hercules Morse, as big as a horse and Mrs Falani were up to no
+              good with a bunch of crook pikelets.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+        <div className="flex flex-col max-w-md sm:mx-auto sm:flex-row">
+          <div className="mr-4">
+            <div className="flex items-center justify-center w-12 h-12 mb-4 rounded-full bg-indigo-50">
+              
+            </div>
+          </div>
+          <div>
+            <h6 className="mb-3 text-xl font-bold leading-5">
+              A slice of heaven
+            </h6>
+            <p className="mb-3 text-sm text-gray-900">
+              Disrupt inspire and think tank, social entrepreneur but
+              preliminary thinking think tank compelling. Inspiring, invest
+              synergy capacity building, white paper; silo, unprecedented
+              challenge B-corp problem-solvers.
+            </p>
+            <a
+              href="/"
+              aria-label=""
+              className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+            >
+              Learn more
+            </a>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  );
+};
