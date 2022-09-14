5967a795c40148534bbd5d64cd8c5fe1c941c4c1
Olvine23 <olivinegeorge@gmail.com>
2022-09-14 08:24:18 +0300

    removed unnused vars

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import one from "./images/backdrop_1.jpg";
+ 
 import "./App.css";
 import { Nav } from "./components/navbar";
 import { Headers } from "./components/hero_two";
-import { Header } from "./components/header";
+ 
 import { Feature } from "./components/features";
 import { Footer } from "./components/footer";
 import { Blog } from "./components/blog";
 import { Card } from "./components/card";
-import { Hero } from "./components/hero";
+ 
 
 function App() {
   return (
