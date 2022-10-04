fd71fa6582f1c9d613b99e50087f1f61d5238a08
Olvine23 <olivinegeorge@gmail.com>
2022-10-04 19:04:00 +0300

    added some grid cols

--- src/components/card.js
+++ src/components/card.js
@@ -1,42 +1,71 @@
 import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
- 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
 export const Card = () => {
   const [showModal, setShowModal] = useState(false);
   return (
-    <div name="destinations" className="  px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+    <div
+      name="destinations"
+      className="  px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20"
+    >
       <div>
-        <h2  className="text-center text-3xl mb-2 font-bold">Destinations</h2>
-        <h3 className="text-center font-bold text-gray-600 divide-y mb-9">Wherever, whenever</h3>
-        <h2 className="text-right mr-6">See All <FontAwesomeIcon icon={faArrowRight} /> </h2>
+        <h2 className="text-center text-3xl mb-2 font-bold">Destinations</h2>
+        <h3 className="text-center font-bold text-gray-600 divide-y mb-9">
+          Wherever, whenever
+        </h3>
+        <h2 className="text-right mr-6">
+          See All <FontAwesomeIcon icon={faArrowRight} />{" "}
+        </h2>
         <div className="grid gap-11   lg:grid-cols-4  sm:max-w-sm sm:mx-auto lg:max-w-full">
           <div className="rounded-2xl shadow-2xl bg-white overflow-hidden">
-             
-            <img 
-          onClick={() => setShowModal(true)}
-            className="  object-cover w-full h-64" alt="t" src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg" />
-            <h1 className=  "mt-3 text-center font-bold text-[#414141]"> <FontAwesomeIcon className="mr-4" icon={faLocationDot} /> Nairobi</h1>
-            
+            <img
+              onClick={() => setShowModal(true)}
+              className="  object-cover w-full h-64"
+              alt="t"
+              src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg"
+            />
+            <h1 className="mt-3 text-center font-bold text-[#414141]">
+              {" "}
+              <FontAwesomeIcon className="mr-4" icon={faLocationDot} /> Nairobi
+            </h1>
           </div>
           <div className="rounded-2xl shadow-2xl bg-white overflow-hidden">
             <img
-            onClick={() => setShowModal(true)}
-            className="object-cover w-full h-64" alt="t" src="https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271_960_720.jpg" />
-            <h1 className=  "mt-3 text-center text-[#414141]"><FontAwesomeIcon className="mr-4" icon={faLocationDot} /> Kisumu</h1>
+              onClick={() => setShowModal(true)}
+              className="object-cover w-full h-64"
+              alt="t"
+              src="https://cdn.pixabay.com/photo/2014/07/10/17/18/large-home-389271_960_720.jpg"
+            />
+            <h1 className="mt-3 text-center text-[#414141]">
+              <FontAwesomeIcon className="mr-4" icon={faLocationDot} /> Kisumu
+            </h1>
           </div>
           <div className="rounded-2xl shadow-2xl bg-white overflow-hidden">
-            <img 
-            onClick={() => setShowModal(true)}
-            className="object-cover w-full h-64" alt="t" src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg" />
-            <h1 className=  "mt-3 text-center font-bold text-[#414141]"> <FontAwesomeIcon className="mr-4" icon={faLocationDot} />Malindi</h1>
+            <img
+              onClick={() => setShowModal(true)}
+              className="object-cover w-full h-64"
+              alt="t"
+              src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg"
+            />
+            <h1 className="mt-3 text-center font-bold text-[#414141]">
+              {" "}
+              <FontAwesomeIcon className="mr-4" icon={faLocationDot} />
+              Malindi
+            </h1>
           </div>
           <div className="rounded-2xl shadow-2xl bg-white overflow-hidden">
-            <img 
-            onClick={() => setShowModal(true)}
-            className="object-cover w-full h-64" alt="t" src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg" />
-            <h1 className=  "mt-3 font-bold text-center text-[#414141]"><FontAwesomeIcon className="mr-4" icon={faLocationDot} />Mombasa</h1>
+            <img
+              onClick={() => setShowModal(true)}
+              className="object-cover w-full h-64"
+              alt="t"
+              src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg"
+            />
+            <h1 className="mt-3 font-bold text-center text-[#414141]">
+              <FontAwesomeIcon className="mr-4" icon={faLocationDot} />
+              Mombasa
+            </h1>
           </div>
         </div>
       </div>
@@ -58,11 +87,32 @@ export const Card = () => {
                   </button>
                 </div>
                 <div className="relative p-6 flex-auto">
-                <img className="object-cover w-full h-64" alt="t" src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg" />
-                <p>This membership package provides studio apartments and one-bedroom apartments; ideal for
-occupancy by a maximum of three individuals for 7-days a year. The package includes transport
-services to and from the airport. The homes are strategically located in safe, quiet areas and with easy
-access to shopping and social amenities.</p> 
+                  <div className="grid grid-cols-2">
+                    <img
+                      className="object-cover w-full h-64"
+                      alt="t"
+                      src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg"
+                    />
+
+                    <img
+                      className="object-cover w-full h-64"
+                      alt="t"
+                      src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg"
+                    />
+                  </div>
+                  <img
+                    className="object-cover w-full h-64"
+                    alt="t"
+                    src="https://cdn.pixabay.com/photo/2016/06/24/10/47/house-1477041__340.jpg"
+                  />
+                  <p>
+                    This membership package provides studio apartments and
+                    one-bedroom apartments; ideal for occupancy by a maximum of
+                    three individuals for 7-days a year. The package includes
+                    transport services to and from the airport. The homes are
+                    strategically located in safe, quiet areas and with easy
+                    access to shopping and social amenities.
+                  </p>
                 </div>
                 <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
                   <button
