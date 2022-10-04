c4520fcd00b201aab9bacad930616f52387ab75c
Olvine23 <olivinegeorge@gmail.com>
2022-10-04 08:44:33 +0300

    added a dropdown menu

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import {AiOutlineSearch} from 'react-icons/ai'
+import MyDropdown from '../menu'
 import Modal from '../modal'
+import Dropdown from './dropdown'
 import './Hero.css'
 
 const Hero = () => {
     return (
         <div className='hero'>
             <div className='content'>
-                <h1>Find the perfect place</h1>
-                <p className='search-text'> Search the largest selection of luxury high-rise apartments, multi-family homes, and luxury homes. </p>
-                <div className='md:mt-7 '>
+                <h1>Welcome To luxelivingairbnbs</h1>
+                <p className='search-text'> Our Luxe club connects you to memorable experiences, specially
+                curated to attend to your personal, family or corporate needs.{" "} </p>
+         <div className='md:mt-7 '>
             <Modal title="View Plans"   />
-             
-            <Modal title="Request"   />
+             <MyDropdown />
+            {/* <Modal title="Request"   /> */}
             </div>
             </div>
         </div>
